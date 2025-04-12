d539a60128356886f0120e1ba3566e49ea41ca12
myhrindra194 <myhrindra194@gmail.com>
2025-04-12 23:12:25 +0300

    _

--- src/layout/HeroSection.jsx
+++ src/layout/HeroSection.jsx
@@ -129,6 +129,7 @@ export default function Home() {
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
+          g
           className="text-center mb-24 pt-12"
         >
           <motion.h1
